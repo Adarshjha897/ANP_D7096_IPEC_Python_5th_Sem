9f966d9eea5a21dd0a7741ab6d5b0c2b13978954
Adarshjha897 <jhaadarsh897@gmail.com>
2026-06-30 23:40:03 +0530

    checking the age is greater or equal to 18 or not

--- classwork/Day6_List30june/AgeValidity.py
+++ classwork/Day6_List30june/AgeValidity.py
@@ -1,28 +1,22 @@
-# Wap to input the age of 15 person for people and displaye the persons as adult whose age is greater = to 18
+# Program to input the age of 15 persons
+# and display the persons as adults (age >= 18)
 
 ages = []
 print("Enter age of 15 persons")
 print("------------------------------------")
 
-for x in range (15):
-#taking input from the user
-     age = int(input())
-    if(age<=0):
-	    print("Invalid age try again")
-	    i = i-1
-        
-    else:
-        ages.append(age)
-        
-#inserting element into the list
+for x in range(15):
+    while True:  # keep asking until valid age is entered
+        age = int(input(f"Enter age of person {x+1}: "))
+        if age <= 0:
+            print("Invalid age, try again")
+        else:
+            ages.append(age)
+            break
 
-print("Entered ages are", ages)
-count = 0
+print("Entered ages are:", ages)
 print("-------------------------------------")
-
+#printing the person who is 18 or above 
 for i in range(15):
-    if(ages[i]>=18):
-        print(f"the person at {i} index is an adult")
-        print("-------------------------------------")
-		
-		
+    if ages[i] >= 18:
+        print(f"The person at index {i} is an adult (age {ages[i]})")
