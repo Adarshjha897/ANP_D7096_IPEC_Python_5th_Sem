7abd3b16be7206da190ee11d2f247d41488a0eb8
Adarshjha897 <jhaadarsh897@gmail.com>
2026-06-30 23:48:11 +0530

    program to check wether the person fall in ews or normal category

--- classwork/Day6_List30june/Salary.py
+++ classwork/Day6_List30june/Salary.py
@@ -1,1 +1,29 @@
-# take the input of person name and salary check wether he fall in Ews cateogry if his salary is less than 
+# take the input of person name and salary check wether he fall in Ews cateogry if his salary is less than 5Lpa
+#-----------------------------------
+
+
+# initializing the list
+record = []
+
+# Input details of 15 persons
+for i in range(15):
+    #taking the name 
+    name = input("Enter Name: ")
+    
+    #taking age 
+    salary = float(input("Enter Salary (in LPA): "))
+    #validating the age
+    while salary <= 0:
+        print("Salary must be positive. Please enter again.")
+        salary = float(input("Enter Salary (in LPA): "))
+    #adding the data into the list
+    record.append([name, salary])
+
+print()
+print("Persons Eligible for EWS Category:")
+
+# Display eligible persons
+for person in record:
+    # Eligible if salary < 5 LPA
+    if person[1] < 5:
+        print(person[0])
