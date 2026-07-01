1b1ed3ccbadd4a7557921846ff73ade463233948
Adarshjha897 <jhaadarsh897@gmail.com>
2026-07-01 12:08:46 +0530

    count metho

--- classwork/Day_7LIst_01JULY/functionList.py
+++ classwork/Day_7LIst_01JULY/functionList.py
@@ -1,7 +1,7 @@
 #funtions available in list
 
 
-numbers = [1,2,3,4,5,6,7,8,9]#list of 10 numbers
+numbers = [1,2,3,4,5,6,7,8,9,5]#list of 10 numbers
 print("-----------------------------------------")
 
 print("The list of numbers is : ", numbers)#printing the list of numbers
@@ -19,3 +19,11 @@ print("The list of numbers after sorting in descending order is : ", numbers.sor
 print("The list of numbers after sorting in ascending order is :", numbers.sort())#printing the numbers in ascending order
 
 print("-----------------------------------------")
+
+
+#count method
+print("The count of 5 in the list is : ", numbers.count(5))
+# when number is not present in the list it will return 0 
+print("The count of 100 in the list is : ", numbers.count(100))
+
+
