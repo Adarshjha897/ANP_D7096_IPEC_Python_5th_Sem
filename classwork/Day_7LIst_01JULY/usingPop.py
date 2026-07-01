c7f99c9e4c156654a9579e59badd199365acd183
Adarshjha897 <jhaadarsh897@gmail.com>
2026-07-01 10:12:18 +0530

    program to explain the use of pop method

---
+++ classwork/Day_7LIst_01JULY/usingPop.py
@@ -0,0 +1,53 @@
+#problem to take input of 10 number in list and then show the use of pop method
+
+
+
+numbers = []
+print("Enter any 10 numbers")
+print("------------------------------------")
+
+for x in range (10):
+#taking input from the user
+
+	num = int(input())
+
+#inserting element into the list
+
+	numbers.append(num)
+
+#-------------------------------------------------
+print("The list of numbers is : ", numbers)
+print("------------------------------------")
+numbers.pop()#it will remove the last element from the list
+print("The list of numbers after using pop without index is : ", numbers)
+print("------------------------------------")
+numbers.pop(2)#it will remove the element of the given index from the list
+print("The list of numbers after using pop with 2 index is : ", numbers)
+
+
+#output:
+"""
+
+Enter any 10 numbers
+------------------------------------
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+The list of numbers is : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+------------------------------------
+The list of numbers after using pop without index is : [1, 2, 3, 4, 5, 6, 7, 8, 9]
+------------------------------------
+The list of numbers after using pop with 2 index is : [1, 2, 4, 5, 6, 7, 8, 9]
+
+
+
+
+"""
