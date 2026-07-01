306ec06b843faa0329b6bdc67326d286fe15edbf
Adarshjha897 <jhaadarsh897@gmail.com>
2026-07-01 11:40:37 +0530

    methods in list with example

---
+++ classwork/Day_7LIst_01JULY/functionList.py
@@ -0,0 +1,21 @@
+#funtions available in list
+
+
+numbers = [1,2,3,4,5,6,7,8,9]#list of 10 numbers
+print("-----------------------------------------")
+
+print("The list of numbers is : ", numbers)#printing the list of numbers
+print("-----------------------------------------")
+
+
+#reverse
+print("The list of numbers after reversing is : ", numbers.reverse()) # after reversing the list of numbers 
+
+print("-----------------------------------------")
+#sort in descending order 
+print("The list of numbers after sorting in descending order is : ", numbers.sort(reverse = True))#printing the numbers in descebding order
+
+#sorting in ascending order
+print("The list of numbers after sorting in ascending order is :", numbers.sort())#printing the numbers in ascending order
+
+print("-----------------------------------------")
