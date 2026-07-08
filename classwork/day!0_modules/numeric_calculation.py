992d949d326acb46595c81e4e70cf66a82c82e60
Adarshjha897 <jhaadarsh897@gmail.com>
2026-07-08 10:39:35 +0530

    module

---
+++ classwork/day!0_modules/numeric_calculation.py
@@ -0,0 +1,22 @@
+#creating module for calulation 
+
+#creating function for addition 
+def calculate_add(num1 , num2):
+    return(num1 + num2)
+
+#creating function of subtraction
+def calculate_sub(num1 , num2):
+    return(num1 - num2)
+
+#creating function for multiplication
+def calculate_mul(num1 , num2):
+    return(num1 * num2)
+
+#creating function for division
+def calculate_div(num1 , num2):
+    return(num1 / num2)
+
+#creating function for remainder
+def calculate_rem(num1 , num2):
+    return(num1 % num2)
+
